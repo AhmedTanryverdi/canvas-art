c8029dca71a9f44f703e2a6808356e7f9b288510
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-15 20:11:23 +0300

    init

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "development",
-	entry: path.resolve(__dirname, "src/index.js"),
+	entry: path.resolve(__dirname, "src/index.ts"),
 
 	devServer: {
 		open: true,
@@ -20,13 +20,28 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.ts?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader",
+					},
+				],
+			},
+			{
 				test: /\.s[ac]ss$/i,
+				exclude: /node_modules/,
 				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 			{
 				test: /\.svg$/,
+				exclude: /node_modules/,
 				use: [{ loader: "file-loader" }],
 			},
 		],
+	},
+
+	resolve: {
+		extensions: [".ts", ".js"],
 	},
 };
