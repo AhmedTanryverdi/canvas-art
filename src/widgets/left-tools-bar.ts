6127c72a1f230108cbaeaea390d91117eb9479d8
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-16 14:26:32 +0300

    created base components

---
+++ src/widgets/left-tools-bar.ts
@@ -0,0 +1,7 @@
+import BaseComponent from "../shared/components/base-component";
+
+export default class LeftToolsBar extends BaseComponent {
+	constructor(id: string) {
+		super("aside", id);
+	}
+}
---
+++ src/widgets/right-tools-bar.ts
@@ -0,0 +1,7 @@
+import BaseComponent from "../shared/components/base-component";
+
+export default class RightToolsBar extends BaseComponent {
+	constructor(id: string) {
+		super("aside", id);
+	}
+}
