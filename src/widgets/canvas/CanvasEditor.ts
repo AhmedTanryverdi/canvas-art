3a281792642bf855df4c1493bccfa6912e793763
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-17 20:53:08 +0300

    added CanvasEditor class for drawing

---
+++ src/widgets/canvas/CanvasEditor.ts
@@ -0,0 +1,42 @@
+export class CanvasEditor {
+	canvas: any;
+	ctx: any;
+	editor: string;
+	isDrawing: boolean;
+	lastX: number | undefined;
+	lastY: number | undefined;
+
+	constructor(canvas: any) {
+		this.canvas = canvas;
+		this.ctx = canvas.getContext("2d");
+		this.editor = "pen";
+		this.isDrawing = false;
+		this.lastX = undefined;
+		this.lastY = undefined;
+
+		this.ctx.lineCap = "round";
+		this.ctx.lineJoin = "round";
+		this.ctx.strokeStyle = "#000"; // Цвет линии
+		this.ctx.lineWidth = 5;
+
+		console.log(this.canvas.width, this.canvas.height);
+	}
+
+	startDraw(e: MouseEvent) {
+		this.isDrawing = true;
+		this.ctx.beginPath();
+		this.ctx.moveTo(e.offsetX, e.offsetY);
+	}
+
+	stopDraw() {
+		this.isDrawing = false;
+		this.ctx.beginPath();
+	}
+
+	drawLine(e: MouseEvent) {
+		if (!this.isDrawing) return;
+
+		this.ctx.lineTo(e.offsetX, e.offsetY);
+		this.ctx.stroke();
+	}
+}
