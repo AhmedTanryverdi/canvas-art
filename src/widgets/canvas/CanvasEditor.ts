755ec0e09c15f005a834dd43f17aac93ad25c3d1
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-18 14:11:40 +0300

    добавил логику редактирования canvas с помощью инструментов pen, eraser, text

--- src/widgets/canvas/CanvasEditor.ts
+++ src/widgets/canvas/CanvasEditor.ts
@@ -1,38 +1,126 @@
-export class CanvasEditor {
-	canvas: any;
-	ctx: any;
-	editor: string;
-	isDrawing: boolean;
+import SingleEditor from "@/shared/lib/SingleEditor";
 
-	constructor(canvas: any) {
+export class CanvasEditor {
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D | null;
+	isDrawing: boolean;
+	textContent: string;
+	xPosition: number;
+	yPosition: number;
+
+	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
 		this.ctx = canvas.getContext("2d");
-		this.editor = "pen";
 		this.isDrawing = false;
+		this.textContent = "";
+		this.xPosition = 10;
+		this.yPosition = 50;
+	}
 
-		this.ctx.lineCap = "round";
-		this.ctx.lineJoin = "round";
-		this.ctx.strokeStyle = "#000"; // Цвет линии
-		this.ctx.lineWidth = 5;
+	renderText(event: KeyboardEvent) {
+		if (!this.ctx || !event.key) return;
+		event.preventDefault();
 
-		console.log(this.canvas.width, this.canvas.height);
+		const lastCharIndex = this.textContent.length - 1;
+		if (event.key === "Backspace") {
+			if (lastCharIndex >= 0) {
+				// Измеряем ширину последнего символа
+				const metrics = this.ctx.measureText(
+					this.textContent[lastCharIndex]
+				);
+
+				// Определяем позицию начала символа относительно общей позиции строки
+				const xPosOfLastSymbol =
+					this.xPosition +
+					this.ctx.measureText(
+						this.textContent.substring(0, lastCharIndex)
+					).width;
+
+				this.textContent = this.textContent.slice(0, -1);
+
+				// Чистка места последнего символа
+				this.ctx.clearRect(
+					xPosOfLastSymbol,
+					this.yPosition,
+					metrics.width,
+					40
+				);
+			}
+		} else if (event.key.length === 1) {
+			this.textContent += event.key;
+		} else if (event.key === "Enter") {
+			this.yPosition += 40;
+			this.textContent = "";
+		}
+
+		// Прорисовываем текст на холсте
+		this.ctx.font = "30px Arial";
+		this.ctx.textBaseline = "top";
+		this.ctx.fillText(this.textContent, this.xPosition, this.yPosition);
 	}
 
 	startDraw(e: MouseEvent) {
 		this.isDrawing = true;
-		this.ctx.beginPath();
-		this.ctx.moveTo(e.offsetX, e.offsetY);
+
+		const editor = SingleEditor.getInstance().getCurrentTool();
+		switch (editor) {
+			case "pen":
+				setPenStart(e, this.ctx);
+				break;
+			case "eraser":
+				this.ctx && setEraserStart(e, this.ctx);
+			case "text":
+				[this.xPosition, this.yPosition] = [e.offsetX, e.offsetY];
+				this.textContent = "";
+		}
 	}
 
 	stopDraw() {
 		this.isDrawing = false;
-		this.ctx.beginPath();
+		const editor = SingleEditor.getInstance().getCurrentTool();
+		switch (editor) {
+			case "pen":
+				this.ctx?.beginPath();
+				break;
+			case "eraser":
+				if (this.isDrawing) this.isDrawing = false;
+		}
 	}
 
 	drawLine(e: MouseEvent) {
 		if (!this.isDrawing) return;
 
-		this.ctx.lineTo(e.offsetX, e.offsetY);
-		this.ctx.stroke();
+		const editor = SingleEditor.getInstance().getCurrentTool();
+		switch (editor) {
+			case "pen":
+				this.ctx?.lineTo(e.offsetX, e.offsetY);
+				this.ctx?.stroke();
+				break;
+			case "eraser":
+				this.ctx && drawEraser(e, this.ctx);
+		}
 	}
+}
+
+function setPenStart(e: MouseEvent, ctx: any) {
+	ctx.lineCap = "round";
+	ctx.lineJoin = "round";
+	ctx.strokeStyle = "#000";
+	ctx.lineWidth = 5;
+
+	ctx.beginPath();
+	ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+function setEraserStart(e: MouseEvent, ctx: CanvasRenderingContext2D) {
+	drawEraser(e, ctx);
+}
+
+function drawEraser(e: MouseEvent, ctx: CanvasRenderingContext2D) {
+	ctx.globalCompositeOperation = "destination-out";
+
+	ctx.beginPath();
+	ctx.arc(e.offsetX, e.offsetY, 20 / 2, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.globalCompositeOperation = "source-over";
 }
