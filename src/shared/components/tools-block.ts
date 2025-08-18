5a14f4d8de94a41f89d206dc73c317d7d7a49cdb
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-18 14:03:07 +0300

    добавил клик для выбора интсрумента рисования

--- src/shared/components/tools-block.ts
+++ src/shared/components/tools-block.ts
@@ -2,6 +2,7 @@ import UIComponent from "./ui-component";
 import eraserSvg from "@/shared/icons/eraser.svg";
 import penSvg from "@/shared/icons/pen.svg";
 import textSvg from "@/shared/icons/text.svg";
+import SingleEditor from "../lib/SingleEditor";
 
 const TOOLITEMS = ["eraser", "pen", "text"];
 
@@ -14,6 +15,11 @@ export class ToolsBlock extends UIComponent {
 	addContent(): void {
 		TOOLITEMS.forEach((element) => {
 			const img = document.createElement("img");
+			img.addEventListener("click", () => {
+				const editor = SingleEditor.getInstance();
+				editor.setCurrentTool(element);
+			});
+			
 			img.classList.add("tool-icon");
 			switch (element) {
 				case "eraser":
