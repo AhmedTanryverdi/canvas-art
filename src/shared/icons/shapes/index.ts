98a184fdaadf5031ea2f720c8b009239274fd84b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-20 19:50:05 +0300

    Добавил .svg файлы

---
+++ src/shared/icons/shapes/index.ts
@@ -0,0 +1,6 @@
+import circleSvg from "./circle.svg";
+import diamindSvg from "./diamond.svg";
+import squareSvg from "./square.svg";
+import triangleSvg from "./triangle.svg";
+
+export { circleSvg, diamindSvg, squareSvg, triangleSvg };
