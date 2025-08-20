483b6fd08b01ebc2d7a8900d9f063ff3eb3b3d86
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-20 14:39:04 +0300

    Рефакторинг. Создал слой app, где в app.ts добавляются компоненты в корневой элемент app

---
+++ src/app/app.ts
@@ -0,0 +1,36 @@
+import { CanvasBoard, LeftPanel } from "@/feature/ui";
+import { Div } from "@/shared/ui";
+import { BASE_COMPONENTS } from "@/shared/utils/constants";
+
+export class App extends Div {
+	create(): HTMLElement | null {
+		this._container = document.getElementById("app");
+		if (!this._container) {
+			this._container = document.createElement("div");
+			if (!this._container) return null;
+			this._container.id = "app";
+		}
+		this.render();
+
+		return this._container;
+	}
+
+	render(): void {
+		if (!this._container) return;
+		BASE_COMPONENTS.forEach((id) => {
+			switch (id) {
+				case "left-panel":
+					const leftPanel = new LeftPanel().create();
+					if (!leftPanel) return null;
+					this._container?.append(leftPanel);
+					break;
+
+				case "canvas":
+					const canvasBoard = new CanvasBoard().create();
+					if (!canvasBoard) return null;
+					this._container?.append(canvasBoard);
+					break;
+			}
+		});
+	}
+}
