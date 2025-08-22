f75648f12f50e75d3329b263dd13e29e5588fe3b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-22 13:04:07 +0300

    добавил случай когда case равно circle и сделал рефакторинг

--- src/feature/canvas-tools/canvas-editor/canvas-editor.ts
+++ src/feature/canvas-tools/canvas-editor/canvas-editor.ts
@@ -1,13 +1,14 @@
 import { PenEditor } from "@/feature/canvas-tools";
 import { EraserEditor } from "@/feature/canvas-tools";
 import { TextEditor } from "@/feature/canvas-tools";
-import { IToolsDraw, IToolsStart, IToolsStop } from "@/shared/utils/types";
+import { IDrawing, IContinuousDrawing } from "@/shared/utils/types";
 import { SingleEditor } from "@/shared/utils/lib/single-editor";
+import { Circle } from "../left-panel/figure/circle";
 
 export class CanvasEditor {
 	private __canvas: HTMLCanvasElement;
 	private __ctx: CanvasRenderingContext2D | null;
-	private __currentTool: (IToolsStart | IToolsStop | IToolsDraw) | null;
+	private __currentTool: (IDrawing | IContinuousDrawing) | null;
 	private __isDrawing: boolean;
 
 	constructor(canvas: HTMLCanvasElement) {
@@ -20,7 +21,8 @@ export class CanvasEditor {
 
 	startDraw(e: MouseEvent) {
 		if (!this.__ctx) return;
-		if (!(this.__currentTool && "startDraw" in this.__currentTool)) return;
+		if (!(this.__currentTool && "startDrawing" in this.__currentTool))
+			return;
 
 		this.__isDrawing = true;
 		const editor = SingleEditor.getInstance();
@@ -32,7 +34,6 @@ export class CanvasEditor {
 			case "pen":
 				tool = new PenEditor(this.__ctx);
 				tool.setThickness(editor.getThicknessTool());
-				this.__currentTool?.startDraw(e);
 				break;
 			case "eraser":
 				tool = new EraserEditor(this.__ctx);
@@ -41,7 +42,9 @@ export class CanvasEditor {
 			case "text":
 				tool = new TextEditor(this.__ctx);
 				tool.setThickness(editor.getThicknessTool());
-				tool.setPosition(e.offsetX, e.offsetY);
+				break;
+			case "circle":
+				tool = new Circle(this.__canvas);
 				break;
 			default:
 				console.error(`Invalid tool name ${currentToolName}`);
@@ -49,28 +52,34 @@ export class CanvasEditor {
 		}
 
 		this.__currentTool = tool;
-		if (!(this.__currentTool && "startDraw" in this.__currentTool)) return;
+		if (!(this.__currentTool && "startDrawing" in this.__currentTool))
+			return;
 
-		this.__currentTool?.startDraw(e);
+		this.__currentTool?.startDrawing.bind(this.__currentTool)(e);
 	}
 
-	stopDraw() {
+	stopDraw(e?: MouseEvent) {
 		this.__isDrawing = false;
 
 		if (!this.__ctx || !this.__currentTool) return;
-		if (!("stopDraw" in this.__currentTool)) return;
 
-		this.__currentTool?.stopDraw(this.__ctx);
+		if ("stopDrawing" in this.__currentTool) {
+			this.__currentTool?.stopDrawing.bind(this.__currentTool)(e!);
+		}
 	}
 
 	drawLine(e: MouseEvent) {
 		if (!this.__isDrawing || !this.__currentTool) return;
 
-		if ("draw" in this.__currentTool) this.__currentTool?.draw(e);
+		if (
+			"drawing" in this.__currentTool &&
+			!(this.__currentTool instanceof TextEditor)
+		)
+			this.__currentTool?.drawing.bind(this.__currentTool)(e);
 	}
 
 	renderText(e: KeyboardEvent) {
-		if (this.__currentTool && "draw" in this.__currentTool)
-			this.__currentTool?.draw(e);
+		if (this.__currentTool && "drawing" in this.__currentTool)
+			this.__currentTool?.drawing(e);
 	}
 }
