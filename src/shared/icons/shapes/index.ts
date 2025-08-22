2dfe9f19faa50a1d6d3faffef9e28d1226fdb2ca
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-22 17:11:12 +0300

    fix. исправил опечатку в diamond

--- src/shared/icons/shapes/index.ts
+++ src/shared/icons/shapes/index.ts
@@ -1,6 +1,6 @@
 import circleSvg from "./circle.svg";
-import diamindSvg from "./diamond.svg";
+import diamondSvg from "./diamond.svg";
 import squareSvg from "./square.svg";
 import triangleSvg from "./triangle.svg";
 
-export { circleSvg, diamindSvg, squareSvg, triangleSvg };
+export { circleSvg, diamondSvg, squareSvg, triangleSvg };
