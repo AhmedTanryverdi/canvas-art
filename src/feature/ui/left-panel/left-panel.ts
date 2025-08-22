5d9c4454eed98d798201a078a6bdfbf3d29d12e6
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-22 13:07:18 +0300

    добавил блок с геометрическими фигурами

--- src/feature/ui/left-panel/left-panel.ts
+++ src/feature/ui/left-panel/left-panel.ts
@@ -1,7 +1,8 @@
 import "./styles.scss";
-import { Aside } from "@/shared/ui";
-import { ToolsBar } from "@/shared/ui";
-import { RangeInput } from "@/shared/ui";
+import { Aside } from "@/shared/ui/aside/aside";
+import { ToolsBar } from "@/shared/ui/tools-bar/tools-bar";
+import { RangeInput } from "@/shared/ui/range-input/range-input";
+import { ShapesBar } from "@/shared/ui/shapes-bar/shapes-bar";
 
 export class LeftPanel extends Aside {
 	create(): HTMLElement | null {
@@ -17,6 +18,10 @@ export class LeftPanel extends Aside {
 		const toolThickness = new RangeInput().create();
 		if (!toolThickness) return null;
 		this._container?.append(toolThickness);
+
+		const shapesBar = new ShapesBar().create();
+		if (!shapesBar) return null;
+		this._container?.append(shapesBar);
 
 		return this._container;
 	}
